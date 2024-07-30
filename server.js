df6f2347edee0425af50b80dfc6e5120b181369a
Jenny Lind <jeli2308@student.miun.se>
2024-07-30 18:47:52 +0200

    Inkluderat npm-paket för jwt-tokens, gjort en skyddad route samt lagt till en funktion som validerar token.

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-// Inkluderar Express, Cors, Mongoose och Dotenv.
+// Inkluderar Express, Cors, Mongoose, Dotenv och jsonwebtoken.
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 // Initialiserar Express.
 const app = express();
@@ -35,6 +36,30 @@ const scoreRoutes = require("./routes/scoreRoutes");
 app.use("/admin", adminRoutes);
 app.use("/icecreams", icecreamRoutes);
 app.use("/scores", scoreRoutes);
+
+// Route för skyddad resurs.
+app.get("/admin/protected", authenticateToken, (req, res) => {
+    res.json({ message: "Åtkomst till admingränssnitt!" });
+});
+
+// Validerar token för åtkomst till skyddat admingränssnitt.
+function authenticateToken(req, res, next) {
+    // Hämtar authorization-header.
+    const authHeader = req.headers["authorization"];
+    // Om headern finns, extraheras token från den.
+    const token = authHeader && authHeader.split(" ")[1];
+
+    // Kontrollerar om en giltig token finns.
+    if(token == null) return res.status(401).json({ message: "Ingen behörighet för skyddat admingränssnitt - token saknas" });
+
+    // Kontrollerar JWT.
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, admin) => {
+        if(err) return res.status(403).json({ message: "Ogiltig JWT" });
+
+        req.admin = admin;
+        next();
+    });
+}
 
 // Startar applikation.
 app.listen(port, () => {
